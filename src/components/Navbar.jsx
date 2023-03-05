93d2508e870d852f7525c0d35da428132b73efab
febrian.rayhan <febrian.rayhan@ui.ac.id>
2023-03-06 00:20:56 +0700

    feat: setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-1/2 mx-auto text-center text-2xl font-bold">
+        <h1>Homepage</h1>
+    </div>
+    
+  )
+}
+
+export default Navbar
